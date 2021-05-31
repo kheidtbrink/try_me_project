42faa855ae0f4d74c47855c4867f51949aff7f74
Kyle Heidtbrink <kheidtbrink@gmail.com>
2021-05-31 14:49:52 +0200

    Initial Push

---
+++ try_me_project/lib.py
@@ -0,0 +1,9 @@
+def invert_string(text_word):
+    """
+    return the text sting in reverse order
+    """
+    # convert decimal degrees to radians
+    new_text = text_word[::-1]
+    return new_text
+
+
